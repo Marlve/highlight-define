2536e654a229c0c59417c8f78679794aac8fd721
Derich <dder0004@student.monash.edu>
2026-07-10 14:32:29 +0800

    FEATURE: Added a HashMap variable to handle caching of words. (resetted every time browser is resetted | max 500 words translation)

--- background.js
+++ background.js
@@ -1,12 +1,31 @@
 import {getTranslation} from "./api.js"
+const cache = new Map()
+const MAX_CACHE_SIZE = 100;
 
 browser.runtime.onMessage.addListener(async (message) => {
   if (message.type == "GET_TRANSLATION") {
     try {
+      if (cache.has(message.word)) {
+        console.log("CACHE EXIST")
+        return {success: true, data: cache.get(message.word)}
+      };
+
       const result = await getTranslation(message.word)
+
+      addToCache(message.word, result);
+
       return {success: true, data: result}
     } catch (err) {
       return {success: false, error: err}
     }
   }
-})
+});
+
+function addToCache(word, result) {
+  cache.set(word, result)
+
+  if (cache.size > MAX_CACHE_SIZE) {
+    const oldestKey = cache.keys().next().value;
+    cache.delete(oldestKey)
+  }
+};
